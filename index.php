23824e30df25363eabc0741bb414e3c476c4ea53
G-Michelle12 <119540874+G-Michelle12@users.noreply.github.com>
2022-12-01 17:13:58 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo '<p>';
+function operaciones_matematicas(){
+  $valor1 = 8;
+  $valor2 = 6;
+  $suma = $valor1 + velor2;
+  $resta = $valor1 - $valor2;
+  $multiplicacion = $valor1 * $valor2;
+  $division = $valor1 / $valor2
+  echo "La suma es: "; print_r($suma);
+  echo '<p>';
+  echo "La resta es: "; print_r($resta);
+  echo '<p>';
+  echo "La multiplicacion es: "; print_r($multiplicacion);
+  echo '<p>';
+  echo "La division es: "; print_r($division);
+}
+operaciones_matematicas();
